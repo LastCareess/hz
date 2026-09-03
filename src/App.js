0831b6c01145ef273a3945eb390ec6188af47f6f
Username <email@example.com>
2026-09-03 19:42:19 +0500

    Integrated api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import { SWRConfig } from "swr";
+import apiConfig from "./api/api-config";
+import MainLayout from "./components/main-layout";
 
 
 function App() {
   return (
-    <p></p>
+    <SWRConfig value={{ fetcher: apiConfig}}>
+      <MainLayout/>
+    </SWRConfig>
   );
 }
 
